d8911ddb24647a16974bd1c2cf8ed808f8bf9a4b
andreinikolabai <andreiplus2000@gmail.com>
2023-03-11 23:52:11 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function sumCyc(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+console.log(`1. Рішення з використанням циклу: ${sumCyc(100)}`);
+
+
+function sumRec(n) {
+    if (n == 1) return 1;
+    return n + sumRec(n - 1);
+}
+
+console.log(`2. Рішення з використанням рекурсії: ${sumRec(100)}`);
